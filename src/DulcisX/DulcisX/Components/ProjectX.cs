0486ed723c276dbb027d85712b720704297c6d35
Twenty <twenty@translucent.at>
2020-01-25 22:00:17 +0100

    Added ProjectX

---
+++ src/DulcisX/DulcisX/Components/ProjectX.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.Shell.Interop;
+
+namespace DulcisX.Components
+{
+    public class ProjectX
+    {
+        public IVsHierarchy UnderlyingHierarchy { get; }
+
+        public ProjectX(IVsHierarchy hierarchy)
+            => UnderlyingHierarchy = hierarchy;
+
+        public void Test()
+        {
+            
+        }
+    }
+}
