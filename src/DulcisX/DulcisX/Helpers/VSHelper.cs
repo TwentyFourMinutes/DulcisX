79f7c6e49fb0b912ef3b829c457264fed3b127bf
Twenty <twenty@translucent.at>
2020-01-26 00:47:10 +0100

    Fixed naming and usings

--- src/DulcisX/DulcisX/Helpers/VSHelper.cs
+++ src/DulcisX/DulcisX/Helpers/VSHelper.cs
@@ -1,9 +1,13 @@
-﻿using DulcisX.Exceptions;
+﻿using DulcisX.Components;
+using DulcisX.Exceptions;
 using Microsoft.VisualStudio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DulcisX.Helpers
 {
-    public static class VSHelper
+    public static class VsHelper
     {
         public static void ValidateVSStatusCode(int statusCode)
         {
