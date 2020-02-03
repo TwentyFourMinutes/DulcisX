114c57e1f5c25618be3a47d40fa6d64e8713e8ad
Twenty <twenty@translucent.at>
2020-02-03 01:08:15 +0100

    Added Document

---
+++ src/DulcisX/DulcisX/Components/DocumentX.cs
@@ -0,0 +1,19 @@
+﻿using DulcisX.Core.Models.Enums;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace DulcisX.Components
+{
+    public class DocumentX : HierarchyItemX
+    {
+        public int BuildAction
+        {
+            get => GetProperty<int>((int)__VSHPROPID4.VSHPROPID_BuildAction);
+            set => SetProperty((int)__VSHPROPID4.VSHPROPID_BuildAction, value);
+        }
+
+        internal DocumentX(IVsHierarchy hierarchy, HierarchyItemX parentItem, uint itemId) : base(hierarchy, parentItem, itemId, HierarchyItemTypeX.Document)
+        {
+
+        }
+    }
+}
