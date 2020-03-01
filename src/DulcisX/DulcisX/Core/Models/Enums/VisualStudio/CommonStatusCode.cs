ca61b0d016578f604c9786e6cb5e592bf1d8f894
Twenty <twenty@translucent.at>
2020-03-01 01:41:02 +0100

    Added CommonStatusCode

---
+++ src/DulcisX/DulcisX/Core/Models/Enums/VisualStudio/CommonStatusCode.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualStudio;
+
+namespace DulcisX.Core.Models.Enums.VisualStudio
+{
+    public static class CommonStatusCode
+    {
+        public const int Success = VSConstants.S_OK;
+        public const int Failure = VSConstants.E_FAIL;
+    }
+}
