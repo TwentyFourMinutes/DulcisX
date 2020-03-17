572417a2f732dd150d0ec9f6ed4b8b94ab9c5d7b
Twenty <twenty@translucent.at>
2020-03-17 17:10:51 +0100

    Added AsyncResult implementation

---
+++ src/DulcisX/DulcisX/Core/AsyncResult.cs
@@ -0,0 +1,30 @@
+﻿namespace DulcisX.Core
+{
+    public class AsyncResult<TResult>
+    {
+        private readonly TResult _result;
+        private readonly bool _hasResult;
+
+        internal AsyncResult(TResult result, bool hasResult)
+        {
+            _result = result;
+            _hasResult = hasResult;
+        }
+
+        public bool TryGetResult(out TResult result)
+        {
+            result = _result;
+
+            return _hasResult;
+        }
+    }
+
+    internal static class AsyncResult
+    {
+        internal static AsyncResult<TResult> FromResult<TResult>(TResult result)
+            => new AsyncResult<TResult>(result, true);
+
+        internal static AsyncResult<TResult> FromError<TResult>()
+            => new AsyncResult<TResult>(default, false);
+    }
+}
